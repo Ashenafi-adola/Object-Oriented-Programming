f481a1e96ad667b7ed51ec015d5e349d23e3ea33
ashenafi <ashenafiadola05@gmail.com>
2025-10-14 12:23:45 +0300

    imported pickle and setted up file

--- Bank management system.py
+++ Bank management system.py
@@ -1,10 +1,18 @@
+import pickle
+FILE_NAME = "PICKLE_FILE.pkl"
+try:
+    with open(FILE_NAME, 'rb') as file:
+        users = pickle.load(file)
+except:
+    users = []
+
 class Account:
     account_number = 1000
     def __init__(self, name, balance, password):
         self.name = name
         self.balance = balance
         self.password = password
-        self.account_number += 1
+        Account.account_number += 1
     
     def deposite(self, deposite):
         if deposite > 0:
@@ -22,6 +30,24 @@ class Account:
     def checkBalance(self):
         print(f"Your balance is {self.balance} Birr")
 
-users = []
-while True:
-    
+    def accountDetails(self):
+        print(f'name {self.name} accout num {self.account_number} balance {self.balance}')
+
+if __name__ == "__main__":
+    while True:
+        print("___________THIS IS THE MAIN PAGE________________")
+        print("|   1. CREATE ACCOUNT                          |")
+        print("|   2. LOGIN TO ACCOUNT                        |")
+        print("|___3._CLOSE___________________________________|")
+        choice = int(input("ENTER YOUR CHOICE: "))
+        if choice == 1:
+            name = input("ENTER YOUR NAME: ")
+            intial = int(input("ENTER YOUR INTIAL DEPOSITE: "))
+            password = input("ENTER YOUR PASSWORD: ")
+            account = Account(name,intial,password)
+            account.accountDetails()
+            users.append(account)
+            with open(FILE_NAME, 'wb') as file:
+                pickle.dump(users,file)
+        elif choice == 3:
+            pass
