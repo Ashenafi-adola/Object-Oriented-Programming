b79127bb727f08a13a2c06fb5dfd7006d0d83a51
ashenafi <ashenafiadola05@gmail.com>
2025-10-15 13:18:52 +0300

    filled class methods in bank management system file

--- Bank system/Bank management system.py
+++ Bank system/Bank management system.py
@@ -2,27 +2,29 @@ import pickle
 FILE_NAME = "PICKLE_FILE.pkl"
 
 class User:
-    def __init__(self, name, initial, password):
+    def __init__(self, name, balance, password):
         self.name = name
-        self.initial = initial
+        self.balance = balance
         self.password = password
     
-    def deposite_money(self):
-        pass
-    def withdraw_money(self):
-        pass
+    def deposite_money(self, deposite):
+        self.balance += deposite
+    def withdraw_money(self, withdraw):
+        self.balance -= withdraw
     def check_balance(self):
-        pass
+        print(f"Your balance is {self.balance} Birr")
     def user_details(self):
-        pass
+        print(f"Name is {self.name} \nBalance is {self.balance}")
 
 class Bank:
     def __init__(self):
         self.users = []
-    def add_user(self):
-        pass
-    def deposite(self):
-        pass
-    def withdraw(self):
-        pass
-
+    def new_user(self, user):
+        self.users.append(user)
+    def deposite(self, user, deposite):
+        user.deposite_money(deposite)
+    def withdraw(self, user, withdraw):
+        user.withdraw_money(withdraw)
+    def show_all_users(self):
+        for user in self.users:
+            print(f"Name is {user.name} \nBalance is {user.balance}")
