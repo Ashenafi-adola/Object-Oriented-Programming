f773bd429f55c731da5208d7057411ee7979b645
ashenafi <ashenafiadola05@gmail.com>
2025-10-15 13:53:47 +0300

    made bank system work

--- Bank system/Bank management system.py
+++ Bank system/Bank management system.py
@@ -21,10 +21,43 @@ class Bank:
         self.users = []
     def new_user(self, user):
         self.users.append(user)
-    def deposite(self, user, deposite):
+    def deposite(self, user):
+        deposite = int(input("Enter Deposite Amount: "))
         user.deposite_money(deposite)
     def withdraw(self, user, withdraw):
         user.withdraw_money(withdraw)
     def show_all_users(self):
         for user in self.users:
             print(f"Name is {user.name} \nBalance is {user.balance}")
+
+bank = Bank()
+
+def create_account():
+    name = input("Enter User's Name: ")
+    password = input("Enter User's Password: ")
+    initial = int(input("Enter initial Deposite: "))
+    return User(name, initial, password)
+
+def get_user(users):
+    name = input("Enter the user name: ")
+    for user in users:
+        if user.name == name:
+            return user
+        
+while True:
+    print(" _______main page________")
+    print("|  1. New Account        |")
+    print("|  2. Deposite           |")
+    print("|  3. Withdraw           |")
+    print("|  4. Balance            |")
+    print("|________________________|")
+    choice = int(input("ENTER YOUR CHOICE: "))
+    match choice:
+        case 1:
+            bank.new_user(create_account())
+        case 2:
+            bank.deposite(get_user(bank.users))
+        case 3:
+            bank.deposite(get_user(bank.users))
+        case 4:
+            get_user(bank.users).check_balance()
